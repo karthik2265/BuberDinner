a0fda40d9ea28249ffc735af02fe114cd5954aa5
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2022-12-05 22:01:05 +0530

    add jwt initial setup

--- BuberDinner.Application/Services/AuthenticationService.cs
+++ BuberDinner.Application/Services/AuthenticationService.cs
@@ -1,7 +1,17 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+  private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+  public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+  {
+    _jwtTokenGenerator = jwtTokenGenerator;
+  }
+
   public AuthenticationResult Login(string email, string password)
   {
     return new AuthenticationResult(Guid.NewGuid(), "fs", "ls", email, "token");
@@ -9,6 +19,12 @@ public class AuthenticationService : IAuthenticationService
 
   public AuthenticationResult Register(string firstName, string lastName, string email, string password)
   {
+    // Check if user already exists
+
+    // Create a user (generate unique Id)
+    var userId = Guid.NewGuid();
+    // Create a JWT token
+    var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
     return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
   }
 }
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Authentication;
+// packages
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace BuberDinner.Application;
 
@@ -6,6 +10,7 @@ public static class DependencyInjection
 {
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
+    services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
     return services;
   }
 }
