a36f6e09aaa438387f03b6143b41befb463b85de
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2023-02-11 14:49:52 +0530

    using middleware

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
   AddApplication().
   AddInfrastructure(builder.Configuration);
 
-  builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
+  builder.Services.AddControllers();
 
 }
 
@@ -20,6 +20,7 @@ var app = builder.Build();
 
 {
   // app.UseMiddleware<ErrorHandlingMiddleware>();
+  app.UseExceptionHandler("/error");
 
   app.UseHttpsRedirection();
 
